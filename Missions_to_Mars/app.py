c024c0668129a129b214eba42975823de7ef6a9c
wawilson810 <wawilson810@gmail.com>
2022-07-11 21:27:24 -0400

    Corrected errors

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars_data = mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data = mars_data)
+
+@app.route("/scrape")
+def scraper():
+    mars_data = mongo.db.mars_data
+    mars_dict = scrape_mars.scrape()
+    mars_data.update_one({}, {"$set": mars_dict}, upsert=True)
+    return redirect("/", code = 302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
-import pandas as pd
-
-app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data"
-mongo = PyMongo(app)
 
 def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -77,18 +69,3 @@ def scrape():
     browser.quit()
 
     return data
-
-@app.route("/")
-def index():
-    mars_data = mongo.db.mars_data.find_one()
-    return render_template("index.html", mars_data = mars_data)
-
-@app.route("/scrape")
-def scraper():
-    mars_data = mongo.db.mars_data
-    mars_dict = scrape()
-    mars_data.update_one({}, {"$set": mars_dict}, upsert=True)
-    return redirect("/", code = 302)
-
-if __name__ == "__main__":
-    app.run(debug=True)
